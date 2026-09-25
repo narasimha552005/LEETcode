a6a7ebfee6cc7f221d8bd00a6c3079b0dd96deca
narasimha <narasimhaswamy123456789123@gmail.com>
2026-09-25 13:07:59 +0530

    Time: 395 ms (76.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION getNthHighestSalary(@N INT)
+RETURNS INT
+AS
+BEGIN
+    RETURN (
+        SELECT MAX(salary)
+        FROM
+        (
+            SELECT
+                salary,
+                DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+            FROM Employee
+        ) AS ranked
+        WHERE rnk = @N
+    );
+END
